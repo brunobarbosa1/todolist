d8dcf641cf76a751ae5af1b6d2868c89ca39aaae
brunobarbosa1 <br.barbosa.souza1@gmail.com>
2026-09-02 23:07:20 -0300

    feat: adiciona a primeira versão das migrations

---
+++ src/main/resources/db/migration/V1__create_table_tblTodo.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tblTodo (
+
+         id BIGSERIAL PRIMARY KEY,
+         title VARCHAR(255) NOT NULL,
+         description VARCHAR(100) NOT NULL,
+         date TIMESTAMP,
+         done BOOLEAN NOT NULL DEFAULT FALSE
+);
